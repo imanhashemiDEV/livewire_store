f14ade36ef1d9d323fede4bae82da0e05e358c30
iman hashemi <hashemi.iman@gmail.com>
2026-07-01 19:54:37 +0330

    users

--- routes/admin.php
+++ routes/admin.php
@@ -3,4 +3,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::livewire('/', 'admin::panel.index');
+Route::livewire('/', 'admin::panel.index')->name('admin.panel.index');
+Route::livewire('/users', 'admin::users.list')->name('admin.users.list');
